04e443b9cbcecc79afddee0e5c5b70ab5c2ecd37
Jokūbas <j4c0bv1g@gmail.com>
2023-09-08 19:25:31 +0300

    Set up server and add config file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('test');
+const express = require('express');
+const config = require('./config');
+
+// Create an instance of the Express application
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Test');
+});
+
+// Start the server
+app.listen(config.serverPort, () => {
+    console.log(`Server is running on port ${config.serverPort}`);
+})
